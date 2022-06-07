6a96937af86af4cb48ef58959ec84506eb16efd8
CamilleMrntz <94170738+CamilleMrntz@users.noreply.github.com>
2022-06-07 22:33:48 +0200

    init

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+
+echo 
+
+    '<div>
+        <p class="prenom">Camille</p>
+    </div>
+    <nav>
+        <ul class="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="realisations.php">Réalisations</a></li>
+            <li><a href="cv.php">CV</a></li>
+        </ul>
+    </nav>'
+
+?>
